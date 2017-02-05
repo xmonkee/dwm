88eb3ffedae6fa6aa4a41cdfc52a6fb9242f62cc
mandava <mandava@uber.com>
2017-02-05 00:22:52 -0800

    Personal config

--- config.def.h
+++ config.def.h
@@ -3,6 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -12,10 +16,11 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_red[]        = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel] =  { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel] =  { col_gray4, col_cyan,  col_red  },
 };
 
 /* tagging */
@@ -27,14 +32,23 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "feh"             , NULL , NULL          , 0      , 1 , -1 } ,
+	{ "Enpass"          , NULL , NULL          , 0      , 1 , -1 } ,
+	{ "Pcmanfm"         , NULL , NULL          , 0      , 1 , -1 } ,
+	{ "Blueman-manager" , NULL , NULL          , 0      , 1 , -1 } ,
+	{ "zoom"            , NULL , NULL          , 0      , 1 , -1 } ,
+	{ "Pavucontrol"     , NULL , NULL          , 0      , 1 , -1 } ,
+	{ "Shutter"         , NULL , NULL          , 0      , 1 , -1 } ,
+	{ "Spotify"         , NULL , NULL          , 1 << 8 , 0 , -1 } ,
+	{ "Firefox"         , NULL , NULL          , 1 << 4 , 0 , -1 } ,
+	{ "Google-chrome"   , NULL , NULL          , 1      , 0 , -1 } ,
+	{ NULL              , NULL , "Preferences" , 0      , 1 , -1 } ,
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -59,10 +73,40 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+static const char *pcmanfm[]        = { "pcmanfm", NULL };
+static const char *lock[]           = { "i3lock", "-d", "-e", "-c", "000000", NULL};
+static const char *shutter[]        = { "shutter", "-s", NULL };
+static const char *pavucontrol[]    = { "pavucontrol", NULL };
+static const char *alsafix[]        = { "alsactl", "init", NULL };
+static const char *monitorfix[]     = { "autorandr", "-c", NULL };
+static const char *volumeup[]       = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL};
+static const char *volumedown[]     = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL};
+static const char *volumemute[]     = {"pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL};
+static const char *brightnessup[]   = {"xbacklight", "-inc", "5", NULL};
+static const char *brightnessdown[] = {"xbacklight", "-dec", "5", NULL};
+static const char *play[]           = { "dbus-send", "--print-reply", "--dest=org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2", "org.mpris.MediaPlayer2.Player.PlayPause", NULL};
+
+
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+
+	{ MODKEY|ShiftMask , XK_o       , spawn          , {.v = lock } }           ,
+	{ MODKEY|ShiftMask , XK_f       , spawn          , {.v = pcmanfm } }        ,
+	{ MODKEY|ShiftMask , XK_t       , spawn          , {.v = shutter } }        ,
+	{ MODKEY|ShiftMask , XK_p       , spawn          , {.v = pavucontrol } }    ,
+	{ MODKEY|ShiftMask , XK_u       , spawn          , {.v = alsafix } }        ,
+	{ MODKEY|ShiftMask , XK_m       , spawn          , {.v = monitorfix } }     ,
+
+    { NULL             , 0x1008ff13 , spawn          , {.v = volumeup } }       ,
+    { NULL             , 0x1008ff11 , spawn          , {.v = volumedown } }     ,
+    { NULL             , 0x1008ff12 , spawn          , {.v = volumemute } }     ,
+    { NULL             , 0x1008ff14 , spawn          , {.v = play } } ,
+    { NULL             , 0x1008ff02 , spawn          , {.v = brightnessup } }   ,
+    { NULL             , 0x1008ff03 , spawn          , {.v = brightnessdown } } ,
+
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -70,9 +114,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -93,7 +139,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
 };
 
 /* button definitions */
