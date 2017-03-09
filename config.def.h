d0ec8b4a4097610ec901b7ffccc9abac3ef9494c
mandava <mandava@uber.com>
2017-03-09 10:40:59 -0800

    color

--- config.def.h
+++ config.def.h
@@ -23,7 +23,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#8EC07C";
+static const char col_cyan[]        = "#005577";
 static const char col_red[]        = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
