801f653fafcd904bf31c17b9018d52b0cbdcade2
mandava <mandava@uber.com>
2017-03-09 10:43:23 -0800

    Change terminal to xfce4

--- config.def.h
+++ config.def.h
@@ -86,7 +86,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "xfce4-terminal", NULL };
 
 static const char *pcmanfm[]        = {"pcmanfm", NULL };
 static const char *lock[]           = {"i3lock", "-d", "-e", "-c", "000000", NULL};
