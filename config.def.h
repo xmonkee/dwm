a879b6bf0ae1286fe28db24b583491d04244a91c
mandava <mandava@uber.com>
2017-03-26 21:40:08 -0700

    Add fancybar

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" , "FontAwesome:size=10"};
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Ubuntu Mono:size=12" , "FontAwesome:size=10"};
+static const char dmenufont[]       = "Ubuntu Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,7 +20,7 @@ static const char col_red[]        = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel] =  { col_gray4, col_gray1,  col_red  },
+	[SchemeSel] =  { col_gray4, col_cyan,  col_red  },
 };
 
 /*Attaching*/
